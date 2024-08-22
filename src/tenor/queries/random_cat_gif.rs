54033f0d9796a5c18ca3bbaefb3e314f42b74085
Nnenty <frag1337n@gmail.com>
2024-08-22 23:42:06 +0300

    add random_cat_gif kinds

--- src/tenor/queries/random_cat_gif.rs
+++ src/tenor/queries/random_cat_gif.rs
@@ -1,13 +1,35 @@
 use crate::tenor::Results;
+use rand::{self, Rng};
 
 use super::{constants::TENOR_RANDOM_QUERY_ADDRESS, errors::QueryError};
 
 pub async fn random_cat_gif_query() -> Result<Results, QueryError> {
+    let rand_query_range = rand::thread_rng().gen_range(0..14);
+
+    let query = match rand_query_range {
+        0 => "cat",
+        1 => "cat+sleep",
+        2 => "cat+meow",
+        3 => "cat+cute",
+        4 => "cool+cat",
+        5 => "cat+meme",
+        6 => "cat+play",
+        7 => "cat+eat",
+        8 => "cat+lying",
+        9 => "cat+sit",
+        10 => "cat+run",
+        11 => "cat+licking",
+        12 => "cat+luna",
+        13 => "cat+drink",
+        14 => "cat+unico",
+        _ => "cat",
+    };
+
     let resp = match reqwest::Client::builder()
         .build()
         .map_err(|err| -> QueryError { err.into() })?
         .get(TENOR_RANDOM_QUERY_ADDRESS)
-        .query(&[("q", "cat"), ("key", "LIVDSRZULELA"), ("limit", "1")])
+        .query(&[("q", query), ("key", "LIVDSRZULELA"), ("limit", "1")])
         .send()
         .await
     {
